808499f7b9d547a8b5d8b0aa515f58b0ecda25d7
r0o0am <83870460+r0o0am@users.noreply.github.com>
2025-06-01 18:28:26 -0300

    remover intervalo de atualização do ranking

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ function initializeRanking() {
     fetchRankingData();
     
     // Atualizar o ranking a cada 30 segundos
-    setInterval(fetchRankingData, 100000);
+    setInterval(fetchRankingData, );
 }
 
 // Buscar dados do ranking da planilha usando SheetDB
